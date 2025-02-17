5d9129822dcd30958afec12fe7752f4f7f347e11
pasmophobia <l@leonmatsuistroemsvik.com>
2025-02-18 03:15:44 +0900

    feat: dbマイグレーション

--- koudaisai-portal-backend/src/db.rs
+++ koudaisai-portal-backend/src/db.rs
@@ -1,9 +1,12 @@
 use sea_orm::{Database, DatabaseConnection, DbErr};
 use tracing::{debug, instrument};
+use migration::{Migrator, MigratorTrait};
 use crate::config::Db;
 
 #[instrument(skip(db))]
 pub async fn init_db(db: &Db) -> Result<DatabaseConnection, DbErr> {
     debug!("Initializing database connection");
-    Database::connect(&db.address).await
+    let db_conn = Database::connect(&db.address).await?;
+    Migrator::up(&db_conn, None).await?;
+    Ok(db_conn)
 }
