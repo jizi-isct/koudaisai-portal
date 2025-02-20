8687e329634681f44f1a99028c0d8f8f34f28ee4
koppe <64785351+github-haruki@users.noreply.github.com>
2025-02-21 00:53:46 +0900

    add: ヘッダー

--- koudaisai-portal-web/src/components/Header/Header.tsx
+++ koudaisai-portal-web/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import styles from "./Header.module.css";
 import Logo from "@/components/Logo/Logo";
+import HeaderButton from "@/components/Button/HeaderButton/HeaderButton";
 
 const Header = () => {
   return (
@@ -9,7 +10,22 @@ const Header = () => {
         <h1 className={styles.title}>工大祭ポータル</h1>
       </div>
       <div className={styles.menuWrapper}>
-        
+        <HeaderButton 
+          text="ホーム"
+          onClick="/" 
+        />
+        <HeaderButton 
+          text="フォーム"
+          onClick="/" 
+        />
+        <HeaderButton 
+          text="資料資料"
+          onClick="/" 
+        />
+        <HeaderButton 
+          text="よくある質問"
+          onClick="/" 
+        />
       </div>
     </header>
   );
