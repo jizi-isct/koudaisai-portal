adb4d385eb7e4f98e92a754348f630f6ba325dfb
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-02-28 07:24:29 +0900

    add: admin data fetch 実装

--- koudaisai-portal-admin/src/app/forms/[formId]/page.tsx
+++ koudaisai-portal-admin/src/app/forms/[formId]/page.tsx
@@ -1,24 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import useSWR from "swr";
+import { use } from "react";
 import TextBox from "@/components/Forms/TextBox/TextBox";
 import Question from "@/components/Forms/Question/Question";
 import Text from "@/components/Forms/Question/Text/Text";
 import CheckBox from "@/components/Forms/Question/CheckBox/CheckBox";
 import RadioButton from "@/components/Forms/Question/RadioButton/RadioButton";
 
-export default async function Page({
-    params,
-  }: {
-    params: Promise<{ formId: string }>
-  }) {
-    const formId = (await params).formId
+export default function Page({ params }: { params: Promise<{ formId: string }> }) {
+  const { formId } = use(params);
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+    const { data, error } = useSWR("http://localhost:4010/api/v1/forms", fetcher);
+    if (error) return <p>データの取得に失敗しました</p>;
+    if (!data) return <p>読み込み中...</p>;
     return (
         <div className={styles.page}>
         <main className={styles.main}>
             <div className={styles.formTitleWrapper}>
-                <h1>工夜祭団体 参加申請フォーム</h1>
-                <p>工夜祭2024を工大祭2日目の17:40~18:40で開催いたします。
-                参加を希望される団体は「工夜祭2024参加募集要項」をご一読の上、このフォームにお答えください。</p>
+                <h1>{data[0].info.title}</h1>
+                <p>{data[0].info.description}</p>
             </div>
             <Question>
               <RadioButton />
