e7dc8acb4611694bd29a3429deb83112d4aceff7
koppe <64785351+github-haruki@users.noreply.github.com>
2025-02-28 23:20:30 +0900

    add: admin item定義

--- koudaisai-portal-admin/src/app/forms/form/page.tsx
+++ koudaisai-portal-admin/src/app/forms/form/page.tsx
@@ -16,24 +16,54 @@ export default function Page() {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error } = useSWR("http://localhost:4010/api/v1/forms", fetcher);
 
+  type Item = {
+    item_id: string;
+    created_at: string;
+    updated_at: string;
+    title: string;
+    description?: string;
+    item_page_break?: object; // ページ区切りアイテム
+    item_text?: object;       // テキストアイテム
+    item_question?: {
+      question: {
+        question_id: string;
+        created_at: string;
+        updated_at: string;
+        required: boolean;
+        question_text: {
+          paragraph: boolean;
+        };
+      };
+    };
+  };
+  
 
-  //useStateでデータ管理
-  const [title, setTitle] = useState("");
+  const [item, setItem] = useState<Item[]>([]);
+
+  
+  useEffect(() => {
+    if (data && Array.isArray(data) && data.length > 0 && data[0]?.items) {
+      console.log("Setting item:", data[0].items);
+      setItem(data[0].items as Item[]);
+    }
+  }, [data]);
 
   if (error) return <p>データの取得に失敗しました</p>;
   if (!data) return <p>読み込み中...</p>;
   
   // form_id に一致するフォームを検索
   // const form = data.find((f: any) => f.form_id === formId);
-  const form = data[0];
+  
   
 
   return (
     <div className={styles.page}>
     <main className={styles.main}>
         <div className={styles.formTitleWrapper}>
-            <p>{form.info.description}</p>
+        <p>{item?.[0]?.item_id ?? "データなし"}</p>
         </div>
+        
+          
         <Question>
           <RadioButton />
         </Question>
