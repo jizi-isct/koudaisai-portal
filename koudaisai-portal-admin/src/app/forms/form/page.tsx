b76c6dae1ba619804ceac0e1f523b3c90ab6ff75
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-02-28 16:11:00 +0900

    fix: admin query parameter

--- koudaisai-portal-admin/src/app/forms/form/page.tsx
+++ koudaisai-portal-admin/src/app/forms/form/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-
-import Image from "next/image";
 import styles from "./page.module.css";
 import useSWR from "swr";
-import { use } from "react";
+import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import TextBox from "@/components/Forms/TextBox/TextBox";
 import Question from "@/components/Forms/Question/Question";
@@ -11,8 +9,9 @@ import Text from "@/components/Forms/Question/Text/Text";
 import CheckBox from "@/components/Forms/Question/CheckBox/CheckBox";
 import RadioButton from "@/components/Forms/Question/RadioButton/RadioButton";
 
-export default function Page({ params }: { params: Promise<{ formId: string }> }) {
-  const { formId } = use{ params }; 
+export default function Page() {
+  const searchParams = useSearchParams();
+  const formId = searchParams.get("formId");
   
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error } = useSWR("http://localhost:4010/api/v1/forms", fetcher);
