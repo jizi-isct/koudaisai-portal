ab46dc57d7f07cdf7f2dc1a5b492678ad573e9e2
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-02-28 14:41:40 +0900

    add: portal 回答送信処理

--- koudaisai-portal-web/src/app/forms/[formId]/page.tsx
+++ koudaisai-portal-web/src/app/forms/[formId]/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { use } from "react";
 import useSWR from "swr";
 import TextQuestion from "@/components/Forms/Questions/TextQuestion/TextQuestion";
+import Button from "@/components/Button/Button";
 
 export default function Page({ params }: { params: Promise<{ formId: string }> }) {
   const { formId } = use{ params }; 
@@ -18,21 +19,39 @@ export default function Page({ params }: { params: Promise<{ formId: string }> }
   const form = data[0];
   const items = form.items;
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4010//api/v1/forms/[formId]/responses", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ info: { title, description }, items: [], access_control: { roles: [] } }),
+    });
+
+    if (response.ok) {
+      alert("フォームを作成しました！");
+    } else {
+      alert("エラーが発生しました。");
+    }
+  };
+
     return (
-        <div className={styles.page}>
         <main className={styles.main}>
             <div className={styles.formTitleWrapper}>
                 <h1>{form.info.title}</h1>
                 <p>{form.info.description}</p>
             </div>
-            <div> 
-                {items.map((item) => {
-                  if (item.item_question != null){
-                    return <TextQuestion key={item.item_question.question.question_id} title={item.title} description={item.description} />;
-                  }
-                })}
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div> 
+                  {items.map((item) => {
+                    if (item.item_question != null){
+                      return <TextQuestion key={item.item_question.question.question_id} title={item.title} description={item.description} />;
+                    }
+                  })}
+              </div>
+              <button type="submit" >送信</button>
+            </form>
+            
         </main>
-        </div>
     );
   }
