f845a8479bc5b38ece153a0e95afa8cce3f50e5b
koppe <64785351+github-haruki@users.noreply.github.com>
2025-03-09 05:04:08 +0900

    add: portal-web authorization header

--- koudaisai-portal-web/src/app/forms/form/page.tsx
+++ koudaisai-portal-web/src/app/forms/form/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import styles from "./page.module.css";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import useSWR from "swr";
 import TextQuestion from "@/components/Forms/Questions/TextQuestion/TextQuestion";
 
@@ -8,8 +10,22 @@ export default function Page() {
   const searchParams = useSearchParams();
   const formId = searchParams.get("formId");
 
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const [authenticated, setAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const access_token = localStorage.getItem('exhibitor_access_token');
+    if (access_token) {
+      setAuthenticated(true);
+    } else {
+      router.push('/login'); // トークンがない場合、ログインページにリダイレクト
+    }
+  }, []);
+
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR("http://localhost:4010/api/v1/forms", fetcher);
+  const { data, error } = useSWR('${API_BASE_URL}/api/v1/forms', fetcher);
 
   if (error) return <p>データの取得に失敗しました</p>;
   if (!data) return <p>読み込み中...</p>;
@@ -24,7 +40,7 @@ export default function Page() {
 
     const response = await fetch("http://localhost:4010//api/v1/forms/[formId]/responses", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json", "Authorization": `Bearer ${access_token}`, },
       body: JSON.stringify({ info: { title, description }, items: [], access_control: { roles: [] } }),
     });
 
