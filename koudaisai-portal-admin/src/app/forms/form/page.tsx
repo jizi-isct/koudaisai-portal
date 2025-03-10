80e7a45e09e42b86414c47fbe700ff03391dfeca
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-03-10 11:38:42 +0900

    add: admin createNewItem

--- koudaisai-portal-admin/src/app/forms/form/page.tsx
+++ koudaisai-portal-admin/src/app/forms/form/page.tsx
@@ -220,6 +220,43 @@ export default function Page() {
     });
   };
 
+  const createNewItem = (itemType: string) => {
+    setForm((prevForm) => {
+      if (!prevForm) return prevForm;
+      
+      const newItem: Item = {
+        item_id: crypto.randomUUID(),
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+        title: "新しい質問",
+        description: "",
+        ...(itemType === "text" && { item_text: {} }),
+        ...(itemType === "page_break" && { item_page_break: {} }),
+        ...(itemType === "question" && {
+          item_question: {
+            question: {
+              question_id: crypto.randomUUID(),
+              created_at: new Date().toISOString(),
+              updated_at: new Date().toISOString(),
+              required: false,
+              question_text: {
+                paragraph: false,
+              },
+            },
+          },
+        }),
+      };
+      
+      const updatedForm = {
+        ...prevForm,
+        items: [...prevForm.items, newItem],
+      };
+      
+      saveForm(updatedForm); // 変更をサーバーに保存
+      return updatedForm;
+    });
+  };
+
   const renderQuestions = () => {
     if (!form || !form.items) return null;
   
@@ -234,6 +271,7 @@ export default function Page() {
       </Question>
     ));
   };
+  
 
 
   if (error) return <p>データの取得に失敗しました</p>;
