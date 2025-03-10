eeedf67d3e095e85f3c449f3c3957cbdad086dad
pasmophobia <l@leonmatsuistroemsvik.com>
2025-03-11 00:29:29 +0900

    docs: openapiを修正

---
+++ docs/update_clients.sh
@@ -0,0 +1,4 @@
+npx openapi-typescript api_v1/openapi.yml --output ../koudaisai-portal-web/src/lib/api_v1.d.ts
+npx openapi-typescript auth_v1/openapi.yml --output ../koudaisai-portal-web/src/lib/auth_v1.d.ts
+npx openapi-typescript api_v1/openapi.yml --output ../koudaisai-portal-admin/src/lib/api_v1.d.ts
+npx openapi-typescript auth_v1/openapi.yml --output ../koudaisai-portal-admin/src/lib/auth_v1.d.ts
