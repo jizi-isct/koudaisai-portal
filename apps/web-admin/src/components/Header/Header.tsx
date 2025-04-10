d084a4cbe330b2a813bc77c6ccdb82a2fafea69d
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-04-10 16:45:18 +0900

    fix: headerbutton componentを削除

--- apps/web-admin/src/components/Header/Header.tsx
+++ apps/web-admin/src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
 import styles from "./Header.module.css";
+import Link from "next/link";
 import Logo from "@/components/Logo/Logo";
 import HeaderButton from "@/components/Button/HeaderButton/HeaderButton";
 
+type HeaderProps = { // ①
+    page: string;
+};
+
 const Header = () => {
+    const buttonStyle ={
+        backgroundColor: 'white',
+        color: 'black',
+        borderColor: '#0048FF'
+    }
     return (
         <header className={styles.header}>
             <div className={styles.logoWrapper}>
                 <Logo height={50} hasText/>
             </div>
             <div className={styles.menuWrapper}>
-                <HeaderButton
-                    text="ホーム"
-                    onClick="/"
-                    isClicked={true}
-                />
+                <Link href="/"><div className={styles.headerButton} style={buttonStyle}>ホーム</div></Link>
                 <HeaderButton
                     text="フォーム"
                     onClick="/forms"
