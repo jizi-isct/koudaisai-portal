4d626b305e7b8b1b6665f1881e59edba45118a65
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-04-11 02:55:09 +0900

    HeaderButtonsのコンポーネントを削除

--- apps/web-admin/src/components/Header/Header.tsx
+++ apps/web-admin/src/components/Header/Header.tsx
@@ -1,37 +1,38 @@
+'use client';
+
 import styles from "./Header.module.css";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import Logo from "@/components/Logo/Logo";
-import HeaderButton from "@/components/Button/HeaderButton/HeaderButton";
 
-type HeaderProps = { // ①
-    page: string;
-};
+const headerItems = [
+    { text: "ホーム", href: "/" },
+    { text: "フォーム", href: "/forms/" },
+    { text: "資料", href: "/documents/" },
+    { text: "よくある質問", href: "/questions/" }
+];
 
 const Header = () => {
-    const buttonStyle ={
-        backgroundColor: 'white',
-        color: 'black',
-        borderColor: '#0048FF'
-    }
+    const pathname = usePathname();
     return (
         <header className={styles.header}>
             <div className={styles.logoWrapper}>
                 <Logo height={50} hasText/>
             </div>
             <div className={styles.menuWrapper}>
-                <Link href="/"><div className={styles.headerButton} style={buttonStyle}>ホーム</div></Link>
-                <HeaderButton
-                    text="フォーム"
-                    onClick="/forms"
-                />
-                <HeaderButton
-                    text="資料"
-                    onClick="/"
-                />
-                <HeaderButton
-                    text="よくある質問"
-                    onClick="/questions"
-                />
+                {headerItems.map(({ text, href }) => {
+                    const isActive = pathname === href;
+
+                    return (
+                    <Link
+                        key={href}
+                        href={href}
+                        className={`${styles.headerNav} ${isActive ? styles.activeNav : styles.inactiveNav}`}
+                    >
+                        {text}
+                    </Link>
+                    );
+                })}
             </div>
         </header>
     );
