24263d105b9b5dcd3ee9580bfacb8d5af17d20a6
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-04-17 22:37:02 +0900

    add: web-adminにフッター読み込み

--- libs/ui-generic/src/Footer/Footer.tsx
+++ libs/ui-generic/src/Footer/Footer.tsx
@@ -18,7 +18,7 @@ export const Footer = () => {
             </div>
             <div className={styles.navWrapper}>
                 <Link href="/" className={styles.navItem}>ホーム</Link>
-                <Link href="/" className={styles.navItem}>お知らせ</Link>
+                <Link href="/questions/" className={styles.navItem}>よくある質問</Link>
                 <Link href="/forms/" className={styles.navItem}>申請一覧</Link>
             </div>
             <p className={styles.copyrightText}>©︎ 2025 JIZI All Rights Reserved.</p>
