2dca420a78c11ba8530aec2c88da32fd2d5238a1
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-04-18 12:17:34 +0900

    fix: web-adminのdivをmainに修正

--- apps/web-admin/src/app/layout.tsx
+++ apps/web-admin/src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
     <html lang="ja">
       <body className={notoSans.className}>
         <Header header_type="admin" />
-        <div className="content">
+        <main className="content">
           {children}
-        </div>
+        </main>
         <Footer />
       </body>
     </html>
