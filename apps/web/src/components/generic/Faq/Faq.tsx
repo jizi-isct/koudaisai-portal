52ba3fc7007a6620015257e0558a11369e703e90
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-05-05 14:35:15 +0900

    fix: numbering questions

--- apps/web/src/components/generic/Faq/Faq.tsx
+++ apps/web/src/components/generic/Faq/Faq.tsx
@@ -1,6 +1,7 @@
 import styles from "./Faq.module.css"
 
 type Props = {
+  number: number
   content: 
   {
     question: string
@@ -8,11 +9,17 @@ type Props = {
   }
 }
 
-export function Faq({content}: Props) {
+export function Faq({number, content}: Props) {
   return (
     <div className={styles.root}>
-      <p className={styles.question}>{content.question}</p>
-      <p className={styles.answer}>{content.answer}</p>
+      <p className={styles.question}>
+        <span className={styles.prefix}>Q{number}. </span>
+        {content.question}
+      </p>
+      <p className={styles.answer}>
+        <span className={styles.prefix}>A{number}. </span>
+        {content.answer}
+      </p>
     </div>
   )
 }
