d6d4a70560ab0d3500fb42fa6267833ceae67dab
pasmophobia <l@leonmatsuistroemsvik.com>
2025-05-07 03:05:41 +0900

    :sparkles: /formsを未公開に
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/app/(withHeaderAndFooter)/forms/page.tsx
+++ apps/web/src/app/(withHeaderAndFooter)/forms/page.tsx
@@ -1,22 +1,22 @@
 "use client";
-import {ContentList, Heading1} from "@/components/generic";
-import {formDataNoLogin} from "@/lib/lib";
+import {Heading1} from "@/components/generic";
 import React from "react";
 
 export default function Page() {
     return (
       <>
         <Heading1 emoji="📃">フォーム一覧</Heading1>
-        <ContentList
-          contents={
-            formDataNoLogin.map((form, i) => ({
-              title: form.title,
-              onClick: () => {
-                window.location.assign(form.url)
-              }
-            }))
-          }
-        />
+        <p>😢ここにはまだ何もありません</p>
+        {/*<ContentList*/}
+        {/*  contents={*/}
+        {/*    formDataNoLogin.map((form, i) => ({*/}
+        {/*      title: form.title,*/}
+        {/*      onClick: () => {*/}
+        {/*        window.location.assign(form.url)*/}
+        {/*      }*/}
+        {/*    }))*/}
+        {/*  }*/}
+        {/*/>*/}
       </>
     )
 }
