66d30095038774127ed04e3a3476e904ad2976cf
pasmophobia <l@leonmatsuistroemsvik.com>
2025-05-12 23:05:51 +0900

    :lipstick: domestic reviews
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/components/Hero/Hero.tsx
+++ apps/web/src/components/Hero/Hero.tsx
@@ -8,7 +8,10 @@ export function Hero() {
         <div className={styles.content}>
           <p className={styles.catchCopy}>一緒に工大祭を創りませんか？</p>
           <h1 className={styles.title}>工大祭2025</h1>
-          <p className={styles.schedule}>テーマ：Bloom 開催日：11/2(日), 11/3(月・祝)</p>
+          <p className={styles.schedule}>
+            テーマ：Bloom<br/>
+            開催日：11/2(日), 11/3(月・祝)
+          </p>
 
           <p className={styles.deadline}>
             参加申込期間：
