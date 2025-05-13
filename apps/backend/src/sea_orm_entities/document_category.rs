7b22dda94b65d8e2ac32c8109c9ef8e91579963b
pasmophobia <l@leonmatsuistroemsvik.com>
2025-05-14 01:27:55 +0900

    :sparkles: add column emoji to table document_category
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/backend/src/sea_orm_entities/document_category.rs
+++ apps/backend/src/sea_orm_entities/document_category.rs
@@ -11,6 +11,8 @@ pub struct Model {
     pub updated_at: Option<DateTimeWithTimeZone>,
     #[sea_orm(column_type = "Text")]
     pub title: String,
+    #[sea_orm(column_type = "Text", nullable)]
+    pub emoji: Option<String>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
