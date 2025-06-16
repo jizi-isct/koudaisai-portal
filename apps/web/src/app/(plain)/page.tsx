34dee3e17c2023439cce3c2fe5cf0b9b8a811acd
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-06-16 23:35:46 +0900

    :sparkles: ExhibitorCardコンポーネントにダウンロードURLを追加し、画像表示を改善

--- apps/web/src/app/(plain)/page.tsx
+++ apps/web/src/app/(plain)/page.tsx
@@ -85,6 +85,7 @@ export default function Page() {
   }, [exhibitor]);
 
 
+
   return (
     <>
       {authenticated ? (
--- apps/web/src/components/exhibitor/ExhibitorCard/ExhibitorCard.tsx
+++ apps/web/src/components/exhibitor/ExhibitorCard/ExhibitorCard.tsx
@@ -1,6 +1,6 @@
 import styles from "./ExhibitorCard.module.css";
 import Image from "next/image";
-import {Exhibitor} from "@/lib";
+import {Exhibitor, useDownloadUrl} from "@/lib";
 import {ButtonCompact} from "@/components/generic/ButtonCompact/ButtonCompact";
 import {useEffect, useState} from "react";
 
@@ -19,9 +19,10 @@ const typeLabels: Record<string, string> = {
 };
 
 export const ExhibitorCard = ({exhibitor, openModal}: ExhibitorCardProps) => {
+    const {downloadUrl, error} = useDownloadUrl(exhibitor?.icon_id, "filename");
     return (
         <div className={styles.card}>
-            <Image src="https://koudaisai.jp/mogi/south/west2.png" alt="Exhibitor Image" width={100} height={100} className={styles.exhibitor_logo} />
+            <Image src={downloadUrl} alt="Exhibitor Image" width={100} height={100} className={styles.exhibitor_logo} />
             <h1>{exhibitor?.exhibition_name}</h1>
             <h4>{typeLabels[exhibitor?.type] || exhibitor?.type}</h4>
             <p>{exhibitor?.description}</p>
