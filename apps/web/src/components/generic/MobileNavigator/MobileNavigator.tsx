c71baac9287532eacd80130a5a69943875661d4d
pasmophobia <l@leonmatsuistroemsvik.com>
2025-06-30 17:55:10 +0900

    :ambulance: スマホ版のログインボタン
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/components/generic/MobileNavigator/MobileNavigator.tsx
+++ apps/web/src/components/generic/MobileNavigator/MobileNavigator.tsx
@@ -3,7 +3,9 @@
 import styles from "./MobileNavigator.module.css";
 import Link from "next/link";
 import {headerItemsAdmin, headerItemsMembers} from "../lib/magicNumbers";
-import {usePathname} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
+import {ButtonCompact} from "@/components/generic/ButtonCompact";
+import {useIsLoggedInMembers} from "@/lib";
 
 
 type Props = {
@@ -11,30 +13,39 @@ type Props = {
 }
 
 export function MobileNavigator({header_type}: Props) {
+  const router = useRouter();
+  const isLoggedIn = useIsLoggedInMembers(); // 仮のログイン状態
   const currentPath = usePathname();
   return (
-    <nav className={`${styles.root} ${header_type === "admin" ? styles.admin : styles.members}`}>
-      {/* ヘッダーのナビゲーションボタン */}
-      {(header_type === "members" ? headerItemsMembers : headerItemsAdmin).map(({
-                                                                                  mobileText,
-                                                                                  emoji,
-                                                                                  href,
-                                                                                  class: className
-                                                                                }) => {
-        // 現在のパスとヘッダーのリンクのパスを比較して、アクティブなリンクを判断
-        const isActive = currentPath === href;
+    <div className={styles.root}>
+      {
+        !isLoggedIn && <div className={styles.login}>
+                <ButtonCompact text={"ログイン"} onClick={() => router.push("/login")}/>
+              </div>
+      }
+      <nav className={`${styles.nav} ${header_type === "admin" ? styles.admin : styles.members}`}>
+        {/* ヘッダーのナビゲーションボタン */}
+        {(header_type === "members" ? headerItemsMembers : headerItemsAdmin).map(({
+                                                                                    mobileText,
+                                                                                    emoji,
+                                                                                    href,
+                                                                                    class: className
+                                                                                  }) => {
+          // 現在のパスとヘッダーのリンクのパスを比較して、アクティブなリンクを判断
+          const isActive = currentPath === href;
 
-        return (
-          <Link
-            key={href}
-            href={href}
-            className={`${styles.headerNav} ${styles[className]} ${isActive ? styles.activeNav : styles.inactiveNav}`}
-          >
-            <span className={styles.emoji}>{emoji}</span>
-            <span className={styles.text}>{mobileText}</span>
-          </Link>
-        );
-      })}
-    </nav>
+          return (
+            <Link
+              key={href}
+              href={href}
+              className={`${styles.headerNav} ${styles[className]} ${isActive ? styles.activeNav : styles.inactiveNav}`}
+            >
+              <span className={styles.emoji}>{emoji}</span>
+              <span className={styles.text}>{mobileText}</span>
+            </Link>
+          );
+        })}
+      </nav>
+    </div>
   )
 }
