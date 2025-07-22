bd0ca8bc5234bed26055c1ed81b3eb19b0c250f3
pasmophobia <l@leonmatsuistroemsvik.com>
2025-07-23 00:09:44 +0900

    :card_file_box: 新しいAPIスキーマ`api_v2`を追加 - OpenAPIに基づいて自動生成された型定義ファイルを`api_v2`として追加 - ドキュメント構造を更新し関連APIの定義を統一
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- docs/build.sh
+++ docs/build.sh
@@ -1,2 +1,2 @@
-redocly build-docs ./api_v1/openapi.yml --output api_v1.html
+redocly build-docs ./api_v2/openapi.yml --output api_v2.html
 redocly build-docs ./auth_v1/openapi.yml --output auth_v1.html
--- docs/bundle.sh
+++ docs/bundle.sh
@@ -1,2 +1,2 @@
-redocly bundle ./api_v1/openapi.yml --output api_v1.yml
+redocly bundle ./api_v2/openapi.yml --output api_v2.yml
 redocly bundle ./auth_v1/openapi.yml --output auth_v1.yml
