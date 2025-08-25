3b0af9ed908463c74169ae8db52a057cc3306516
pasmophobia <l@leonmatsuistroemsvik.com>
2025-08-26 01:18:27 +0900

    :sparkles: フロントエンドの通知表示を新しいデータ形式に対応
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/components/notification/ContentRowNotification/index.tsx
+++ apps/web/src/components/notification/ContentRowNotification/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import {ContentRow} from "@/components/generic/ContentRow";
 import {NotificationRead} from "@/lib";
-import {useState} from "react";
-import {Modal} from "@/components/generic";
-import Markdown from "react-markdown";
+import {ContentRowNotificationTypeMarkdown} from "@/components/notification/ContentRowNotificationTypeMarkdown";
+import {
+  ContentRowNotificationTypeApprovalRequest
+} from "@/components/notification/ContentRowNotificationTypeApprovalRequest";
 
 export type Content = {
   title: string,
@@ -18,28 +18,12 @@ type ContentRowNotificationProps = {
 }
 
 export function ContentRowNotification({notification}: ContentRowNotificationProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return (
-    <>
-      <ContentRow
-        content={
-          {
-            title: notification.title,
-            date: new Date(notification.created_at)
-            .toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '.'),
-            onClick: () => {
-              setIsModalOpen(true)
-            }
-          }
-        }
-      />
-      <Modal isOpen={isModalOpen} setOpen={setIsModalOpen}>
-        <h1>{notification.title}</h1>
-        <Markdown>
-          {notification.type_markdown.content}
-        </Markdown>
-      </Modal>
-    </>
-  )
+  if ("type_markdown" in notification) {
+    return <ContentRowNotificationTypeMarkdown notification={notification} markdown={notification.type_markdown}/>
+  } else if ("type_approval_request" in notification) {
+    return <ContentRowNotificationTypeApprovalRequest notification={notification}
+                                                      notificationApprovalRequest={notification.type_approval_request}/>
+  } else {
+    return <></>
+  }
 }
