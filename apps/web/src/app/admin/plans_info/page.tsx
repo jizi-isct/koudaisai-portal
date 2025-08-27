94e9fce21d537c73fea24432d107fdcd406422d8
pasmophobia <l@leonmatsuistroemsvik.com>
2025-08-27 14:13:31 +0900

    :bug: bulk update時のロールバック処理を無効化
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/app/admin/plans_info/page.tsx
+++ apps/web/src/app/admin/plans_info/page.tsx
@@ -494,29 +494,11 @@ function Inner() {
         }))
 
         if (isError) {
-          messageApi.loading({
-            content: "ロールバック中... ブラウザを閉じないでください．",
-            key: hash + "_rollback",
-            duration: 0
-          })
-
-          for (const id of isCreated) {
-            try {
-              await mutatePlanDelete({
-                params: {
-                  path: {
-                    planId: id
-                  }
-                },
-              })
-            } catch (e) {
-              messageApi.destroy(`${id} の削除に失敗しました${JSON.stringify(e)}`)
-            }
-          }
-          messageApi.destroy(hash + "_rollback")
-          messageApi.info({
-            content: "ロールバック処理が終了しました．",
-          })
+          messageApi.destroy(hash)
+          messageApi.error({
+            content: `一部の更新に失敗しました。${isCreated.length}件更新しました。反映には最長で１分ほどかかる可能性があります。`,
+            key: hash
+          });
         } else {
           messageApi.destroy(hash)
           messageApi.success({
