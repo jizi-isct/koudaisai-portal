43f05afcfecbee3093208c67ae2be5b9f422cb7c
pasmophobia <l@leonmatsuistroemsvik.com>
2025-08-27 14:28:44 +0900

    :sparkles: ダッシュボードにグループidを表示(fix #156)
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/components/UserInfoCard/UserInfoCard.tsx
+++ apps/web/src/components/UserInfoCard/UserInfoCard.tsx
@@ -14,27 +14,27 @@ export const UserInfoCard = ({user, group}: UserInfoCardProps) => {
       <h1>こんにちは、{user.name} 👋</h1>
       {
         group.type_plan?.type_booth && (
-          <h2>あなたは{group.name}の第{representativeIndex}責任者です。</h2>
+          <h2>あなたは{group.name}<small>({group.id})</small>の第{representativeIndex}責任者です。</h2>
         )
       }
       {
         group.type_plan?.type_general && (
-          <h2>あなたは{group.name}の第{representativeIndex}責任者です。</h2>
+          <h2>あなたは{group.name}<small>({group.id})</small>の第{representativeIndex}責任者です。</h2>
         )
       }
       {
         group.type_plan?.type_stage && (
-          <h2>あなたは{group.name}の第{representativeIndex}責任者です。</h2>
+          <h2>あなたは{group.name}<small>({group.id})</small>の第{representativeIndex}責任者です。</h2>
         )
       }
       {
         group.type_plan?.type_labo && (
-          <h2>あなたは{group.name}の企画実施担当者です。</h2>
+          <h2>あなたは{group.name}<small>({group.id})</small>の企画実施担当者です。</h2>
         )
       }
       {
         group.type_press && (
-          <h2>あなたは{group.name}の代表者です。</h2>
+          <h2>あなたは{group.name}<small>({group.id})</small>の代表者です。</h2>
         )
       }
     </div>
