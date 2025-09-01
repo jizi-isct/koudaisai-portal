cfd779f2c3670cc546b2108cb14e5aa981e0a57e
koppepan3 <64785351+koppepan3@users.noreply.github.com>
2025-09-01 16:03:25 +0900

    :art: EditPendingFormコンポーネントでButtonCompactをButtonに変更し、コードを整理

--- apps/web/src/components/plan/EditPendingForm/index.tsx
+++ apps/web/src/components/plan/EditPendingForm/index.tsx
@@ -1,35 +1,36 @@
-'use client'; // クライアントサイドコンポーネントとして実行するために追加
+"use client"; // クライアントサイドコンポーネントとして実行するために追加
 
 import React from "react";
-import {$apiMembers, ApprovalRequestRead} from "@/lib";
-import {ButtonCompact} from "@/components/generic/ButtonCompact";
-import {Heading1, LoadingScreen} from "@/components/generic";
-import {Loader} from "@/components/generic/Loader";
-import {ViewPendingEditExhibitionInfoRequest} from "@/components/plan/ViewPendingEditExhibitionInfoRequest";
-import {$plansInfoApiNoLogin} from "@/lib/plansInfoApi";
-
+import { $apiMembers, ApprovalRequestRead } from "@/lib";
+import { Button } from "antd";
+import { Heading1, LoadingScreen } from "@/components/generic";
+import { ViewPendingEditExhibitionInfoRequest } from "@/components/plan/ViewPendingEditExhibitionInfoRequest";
+import { $plansInfoApiNoLogin } from "@/lib/plansInfoApi";
 
 type Props = {
   refetch: () => Promise<void>;
   planId: string;
-  approvalRequest: ApprovalRequestRead
+  approvalRequest: ApprovalRequestRead;
 };
 
-export function EditPendingForm({
-                                  refetch,
-                                  planId,
-                                  approvalRequest
-                                }: Props) {
-  const {mutateAsync: closeApprovalRequest} = $apiMembers.useMutation("post", "/users/{user_id}/approval_requests/{request_id}/close")
+export function EditPendingForm({ refetch, planId, approvalRequest }: Props) {
+  const { mutateAsync: closeApprovalRequest } = $apiMembers.useMutation(
+    "post",
+    "/users/{user_id}/approval_requests/{request_id}/close"
+  );
   const [isClosing, setIsClosing] = React.useState<boolean>(false);
 
-  const {data: plan} = $plansInfoApiNoLogin.useQuery("get", "/plans/{planId}", {
-    params: {
-      path: {
-        planId: planId
-      }
+  const { data: plan } = $plansInfoApiNoLogin.useQuery(
+    "get",
+    "/plans/{planId}",
+    {
+      params: {
+        path: {
+          planId: planId,
+        },
+      },
     }
-  })
+  );
 
   const handleClose = async () => {
     setIsClosing(true);
@@ -37,25 +38,34 @@ export function EditPendingForm({
       params: {
         path: {
           user_id: "me",
-          request_id: approvalRequest.id
-        }
-      }
-    })
+          request_id: approvalRequest.id,
+        },
+      },
+    });
     setIsClosing(false);
     await refetch();
-  }
+  };
 
   if (!plan) {
-    return <LoadingScreen/>
+    return <LoadingScreen />;
   }
 
-  return (<div>
-    <Heading1 emoji={"📝"}>企画情報訂正申請は現在審査中です</Heading1>
+  return (
+    <div>
+      <Heading1 emoji={"📝"}>企画情報訂正申請は現在審査中です</Heading1>
 
-    <ViewPendingEditExhibitionInfoRequest approvalRequest={approvalRequest} plan={plan}/>
-    {
-      isClosing ? <Loader/> :
-        <ButtonCompact text={"企画情報訂正申請を取り下げる"} onClick={handleClose}/>
-    }
-  </div>)
+      <ViewPendingEditExhibitionInfoRequest
+        approvalRequest={approvalRequest}
+        plan={plan}
+      />
+      <Button
+        type="primary"
+        style={{ alignSelf: "flex-start" }}
+        loading={isClosing}
+        onClick={handleClose}
+      >
+        企画情報訂正申請を取り下げる
+      </Button>
+    </div>
+  );
 }
