68ce15ecab9b67501902ec82bc4784c0baa7fbf5
pasmophobia <l@leonmatsuistroemsvik.com>
2025-10-31 15:33:58 +0900

    :sparkles: EditAdditionalInfoに更新ボタンとローディング状態を追加
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/web/src/components/plan-details/additional-info/EditAdditionalInfo/index.tsx
+++ apps/web/src/components/plan-details/additional-info/EditAdditionalInfo/index.tsx
@@ -29,15 +29,18 @@ import {
   AdmonitionDirectiveDescriptor, imagePlugin, SingleChoiceToggleGroup
 } from "@mdxeditor/editor";
 import {Kiwi_Maru} from 'next/font/google';
-import {JSX, useState} from "react";
-import {AppstoreOutlined, CompassOutlined} from "@ant-design/icons";
+import {useState} from "react";
+import {AppstoreOutlined, CompassOutlined, LoadingOutlined, UploadOutlined} from "@ant-design/icons";
+import {Button} from "antd";
 
+// フォント
 const kiwiMaru = Kiwi_Maru({
   weight: ['300', '400', '500'],
   subsets: ['latin'],
   display: 'swap'
 });
 
+// 表示切り替えのリスト
 const styleChoiceItems = [
   {
     title: "アプリ表示",
@@ -51,23 +54,36 @@ const styleChoiceItems = [
   }
 ]
 
-export function EditAdditionalInfo() {
+type Props = {
+  additionalInfo: string,
+  updateAdditionalInfo: (newAdditionalInfo: string) => Promise<void>,
+  isLoading: boolean,
+}
+
+export function EditAdditionalInfo({additionalInfo, updateAdditionalInfo, isLoading}: Props) {
   const [style, setStyle] = useState<"app" | "web">("app")
+  const [currentMarkdown, setCurrentMarkdown] = useState<string>(additionalInfo)
 
   return (
     <div className={styles.editorWrapper}>
       <MDXEditor
-        markdown="Hello world"
+        markdown={additionalInfo}
+        onChange={(md) => setCurrentMarkdown(md)}
         contentEditableClassName={style === "app" ? `${styles.app} ${kiwiMaru.className}` : styles.web}
         plugins={[
           toolbarPlugin({
             toolbarContents: () => (
               <DiffSourceToggleWrapper>
-                <UndoRedo/>
+                <Button
+                  size={"small"}
+                  disabled={isLoading || currentMarkdown === additionalInfo}
+                  onClick={async () => {await updateAdditionalInfo(currentMarkdown)}}
+                >{isLoading ? <LoadingOutlined/> : <UploadOutlined/>} 更新</Button>
                 <Separator/>
                 <SingleChoiceToggleGroup items={styleChoiceItems} onChange={(v) => setStyle(v as "app" | "web")}
                                          value={style}/>
                 <Separator/>
+                <UndoRedo/>
                 <BlockTypeSelect/>
                 <Separator/>
                 <BoldItalicUnderlineToggles/>
@@ -84,7 +100,7 @@ export function EditAdditionalInfo() {
             )
           }),
           imagePlugin(),
-          diffSourcePlugin({viewMode: 'rich-text'}),
+          diffSourcePlugin({diffMarkdown: additionalInfo, viewMode: 'rich-text'}),
           headingsPlugin({allowedHeadingLevels: [1, 2, 3, 4, 5, 6]}),
           listsPlugin(),
           tablePlugin(),
