95c69c2e4f8a393391e694043eb2683e6667e96f
pasmophobia <l@leonmatsuistroemsvik.com>
2026-01-22 19:52:37 +0900

    :recycle: `UserId`に`Display`トレイトを実装し、タプル定義に変更
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- apps/backend/src/domain/user_id.rs
+++ apps/backend/src/domain/user_id.rs
@@ -1,12 +1,17 @@
+use std::fmt::Display;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
-pub struct UserId {
-    pub id: Uuid
-}
+pub struct UserId(Uuid);
 
 impl UserId {
     pub fn new(id: Uuid) -> Self {
-        Self { id }
+        Self(id)
+    }
+}
+
+impl Display for UserId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
