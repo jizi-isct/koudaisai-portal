a18f4bab987a65806a1ece7f9cf226a7d667e1b2
25-RinaOi <rinaoi4869@gmail.com>
2026-02-12 02:20:06 +0900

    util.tsを分割しました.tsconfig.base.jsonにパスを追加しました
    # Conflicts:
    #	package-lock.json

---
+++ libs/shared-utils/src/file.ts
@@ -0,0 +1,58 @@
+// apps/web/src/lib/util.ts から以下の関数をコピー
+import {fetchClientNoAuth} from "@/lib/api";
+import {useCallback, useEffect, useState} from "react";
+
+export async function getDownloadUrl(fileKey: string, fileName: string) {
+  return await fetchClientNoAuth.GET(
+    "/files/download",
+    {
+      params: {
+        query: {
+          key: fileKey,
+          file_name: fileName
+        }
+      }
+    }
+  )
+}
+
+export function useDownloadUrl(fileKey: string, fileName: string) {
+  const [downloadUrl, setDownloadUrl] = useState<string | undefined>(undefined);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const {data, error: error_} = await getDownloadUrl(fileKey, fileName)
+      if (data) {
+        setDownloadUrl(data.presigned_url);
+      } else {
+        setError(`Error fetching download URL: ${error_}`);
+      }
+    })()
+  })
+
+  return {
+    downloadUrl,
+    error
+  }
+}
+
+export function useDownload() {
+  return useCallback(
+    (url: string, fileName: string) => {
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = fileName;
+      a.style.display = "none";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      URL.revokeObjectURL(url);
+    }, []
+  );
+}
+
+export function getFilesRedirectUrl(fileKey: string) {
+  return `${process.env.NEXT_PUBLIC_API_BASE_URL}/files/download?key=${fileKey}&file_name=${fileKey}&redirect=true`
+}
---
+++ libs/shared-utils/src/representative.ts
@@ -0,0 +1,42 @@
+/**
+ * ユーザーIDから代表者のインデックスを取得
+ */
+import {GroupRead, UserRead} from "@/lib/types";
+
+export function getRepresentativeIndex(user: UserRead, group: GroupRead): string | undefined {
+  if (group.type_press) {
+    return undefined
+  }
+  if (group.type_plan?.type_booth) {
+    if (group.type_plan.type_booth.representative1 === user.id) {
+      return "一";
+    } else if (group.type_plan.type_booth.representative2 === user.id) {
+      return "二";
+    } else if (group.type_plan.type_booth.representative3 === user.id) {
+      return "三";
+    }
+  }
+  if (group.type_plan?.type_general) {
+    if (group.type_plan.type_general.representative1 === user.id) {
+      return "一";
+    } else if (group.type_plan.type_general.representative2 === user.id) {
+      return "二";
+    } else if (group.type_plan.type_general.representative3 === user.id) {
+      return "三";
+    }
+  }
+  if (group.type_plan?.type_stage) {
+    if (group.type_plan.type_stage.representative1 === user.id) {
+      return "一";
+    } else if (group.type_plan.type_stage.representative2 === user.id) {
+      return "二";
+    } else if (group.type_plan.type_stage.representative3 === user.id) {
+      return "三";
+    }
+  }
+  if (group.type_plan?.type_labo) {
+    return undefined
+  }
+  return undefined
+}
+
