8ef70b34cc91748aae82bcaa776c591eb9692188
pasmophobia <l@leonmatsuistroemsvik.com>
2026-03-13 15:51:57 +0900

    shared-typesのビルドエラーを修正
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

--- libs/shared-types/src/index.ts
+++ libs/shared-types/src/index.ts
@@ -1,1 +1,11 @@
-export * from './types';
+export type * from './types';
+export {
+  type paths as apiPaths,
+  type webhooks as apiWebhooks,
+  type components as apiComponents,
+  type $defs as $apiDefs,
+  type operations as apiOperations,
+} from "./api_v2";
+export {
+  type paths as authPaths
+} from "./auth_v1";
