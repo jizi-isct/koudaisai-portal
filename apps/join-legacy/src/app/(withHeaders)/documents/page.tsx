b245bb21ed0db8779e32acef00ce5bb4cabb73e0
pasmophobia <l@leonmatsuistroemsvik.com>
2026-05-13 19:39:23 +0900

    Add join legacy app

---
+++ apps/join-legacy/src/app/(withHeaders)/documents/page.tsx
@@ -0,0 +1,23 @@
+import {getApiFetchClient} from "@koudaisai/shared-api";
+import ViewDocumentsWrapper from "../../../components/ViewDocumentsWrapper";
+import {Heading1} from "@koudaisai/shared-ui";
+import styles from "./page.module.css";
+
+export default async function Page_() {
+  const fetchClient = getApiFetchClient("https://portal.koudaisai.jp/api/v2")
+
+  // 資料一覧の取得
+  const {data, error} = await fetchClient.GET("/documents/by-category")
+  if (error || !data) {
+    throw error;
+  }
+
+  return (
+    <main className={styles.main}>
+      <Heading1 emoji={"📚"}>資料</Heading1>
+      <div className={styles.container}>
+        <ViewDocumentsWrapper documents={data}/>
+      </div>
+    </main>
+  )
+}
---
+++ apps/join-legacy/src/app/(withHeaders)/forms/page.tsx
@@ -0,0 +1,23 @@
+import styles from "../documents/page.module.css";
+import {Heading1} from "@koudaisai/shared-ui";
+import {getApiFetchClient} from "@koudaisai/shared-api";
+import {ViewFormCards} from "@koudaisai-portal/shared-ui-forms";
+
+export default async function Page() {
+  const fetchClient = getApiFetchClient("https://portal.koudaisai.jp/api/v2")
+
+  // フォーム一覧の取得
+  const {data, error} = await fetchClient.GET("/forms")
+  if (error || !data) {
+    throw error;
+  }
+
+  return (
+    <main className={styles.main}>
+      <Heading1 emoji={"📝"}>フォーム</Heading1>
+      <div className={styles.container}>
+        <ViewFormCards forms={data}/>
+      </div>
+    </main>
+  )
+}
