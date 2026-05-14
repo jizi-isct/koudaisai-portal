5884677e62a2fc83364403339a6de6cb13f4b4e7
pasmophobia <l@leonmatsuistroemsvik.com>
2026-05-14 14:54:01 +0900

    feat(join): migrate header to Astro

---
+++ apps/join/src/features/header/navItems.ts
@@ -0,0 +1,6 @@
+export const headerItems = [
+  { desktopText: "ホーム", mobileText: "ホーム", href: "/", className: "navHome" },
+  { desktopText: "フォーム", mobileText: "フォーム", href: "/forms/", className: "navForm" },
+  { desktopText: "資料", mobileText: "資料", href: "/documents/", className: "navDocuments" },
+  { desktopText: "よくある質問", mobileText: "FAQ", href: "/questions/", className: "navQuestions" },
+] as const;
