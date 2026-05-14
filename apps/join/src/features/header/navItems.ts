8e450e68049fcce22ec709ee97b60b618c7e959c
pasmophobia <l@leonmatsuistroemsvik.com>
2026-05-15 00:35:29 +0900

    Add Astro mobile navigator to join app

--- apps/join/src/features/header/navItems.ts
+++ apps/join/src/features/header/navItems.ts
@@ -1,6 +1,6 @@
 export const headerItems = [
-  { desktopText: "ホーム", mobileText: "ホーム", href: "/", className: "navHome" },
-  { desktopText: "フォーム", mobileText: "フォーム", href: "/forms/", className: "navForm" },
-  { desktopText: "資料", mobileText: "資料", href: "/documents/", className: "navDocuments" },
-  { desktopText: "よくある質問", mobileText: "FAQ", href: "/questions/", className: "navQuestions" },
+  { desktopText: "ホーム", mobileText: "ホーム", emoji: "🏠", href: "/", className: "navHome" },
+  { desktopText: "フォーム", mobileText: "フォーム", emoji: "📄", href: "/forms/", className: "navForm" },
+  { desktopText: "資料", mobileText: "資料", emoji: "📚", href: "/documents/", className: "navDocuments" },
+  { desktopText: "よくある質問", mobileText: "FAQ", emoji: "❓", href: "/questions/", className: "navQuestions" },
 ] as const;
