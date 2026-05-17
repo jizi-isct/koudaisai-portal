6c98a529a5deadb45ad678751ddc131fd777f2ed
pasmophobia <l@leonmatsuistroemsvik.com>
2026-05-17 21:39:15 +0900

    feat(admin): API クライアントと認証ミドルウェアを追加 🛠️
    Signed-off-by: pasmophobia <l@leonmatsuistroemsvik.com>

---
+++ apps/admin/src/features/api/api.ts
@@ -0,0 +1,17 @@
+import {getAuthMiddleware} from "@koudaisai/shared-auth-admin";
+import {
+  getApiFetchClient,
+  getApiQueryClient,
+  getPlansInfoFetchClient,
+  getPlansInfoQueryClient
+} from "@koudaisai/shared-api";
+import {API_URL, AUTH_URL, PLANS_INFO_API_URL} from "astro:env/client";
+import {getAuthFetchClient} from "@koudaisai/shared-auth";
+
+export const authFetchClient = getAuthFetchClient(AUTH_URL);
+const authMiddleware = getAuthMiddleware(authFetchClient);
+export const api = getApiFetchClient(API_URL, authMiddleware);
+export const $api = getApiQueryClient(api);
+
+export const plansInfoApi = getPlansInfoFetchClient(PLANS_INFO_API_URL, authMiddleware)
+export const $plansInfoApi = getPlansInfoQueryClient(plansInfoApi);
