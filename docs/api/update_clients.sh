023ff6b38b42dc63ee6c8bcef6d4505023b603bd
pasmophobia <l@leonmatsuistroemsvik.com>
2026-05-20 13:09:51 +0900

    chore: remove legacy web projects
    # Conflicts:
    #	package-lock.json

--- docs/api/update_clients.sh
+++ docs/api/update_clients.sh
@@ -1,3 +1,3 @@
-npx openapi-typescript api_v2/openapi.yml --output ../apps/web/src/lib/api_v2.d.ts
-npx openapi-typescript auth_v1/openapi.yml --output ../apps/web/src/lib/auth_v1.d.ts
-npx openapi-typescript https://raw.githubusercontent.com/jizi-isct/koudaisai-plans-info-api/refs/heads/main/docs/openapi.yml --output ../apps/web/src/lib/plans_info_api_v1.d.ts
+npx openapi-typescript api_v2/openapi.yml --output ../../libs/shared-types/src/api_v2.d.ts
+npx openapi-typescript auth_v1/openapi.yml --output ../../libs/shared-types/src/auth_v1.d.ts
+npx openapi-typescript https://raw.githubusercontent.com/jizi-isct/koudaisai-plans-info-api/refs/heads/main/docs/openapi.yml --output ../../libs/shared-types/src/plans_info_api_v1.d.ts
