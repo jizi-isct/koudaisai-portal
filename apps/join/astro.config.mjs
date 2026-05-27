8cd2f320304d789efd4ba70df5653d5099f1bfe7
FlyingWorld <koichi323530nagaoka@gmail.com>
2026-05-27 23:06:26 +0900

    astro.config.mjsにsiteパラメータを追加．

--- apps/join/astro.config.mjs
+++ apps/join/astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   output: 'static',
+  site: "https://join.koudaisai.jp",
   env: {
     schema: {
       API_URL: envField.string({ context: 'client', access: 'public' }),
