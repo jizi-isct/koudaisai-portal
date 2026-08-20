2fb9fbdbfa9edac201c562d13149af264a92d2b8
pasmophobia <l@leonmatsuistroemsvik.com>
2026-08-20 19:09:35 +0200

    feat(domain): define a new domain object `Settings`

---
+++ apps/backend/src/domain/settings.rs
@@ -0,0 +1,31 @@
+/// admin ユーザーが変更可能な設定
+#[derive(Debug, Clone, PartialEq)]
+pub struct Settings {
+    show_occasions_on_portal: bool,
+}
+
+impl Settings {
+    /// 初期化用コンストラクタ
+    pub fn new(show_occasions_on_portal: bool) -> Self {
+        Self {
+            show_occasions_on_portal,
+        }
+    }
+
+    /// db 復元用コンストラクタ
+    pub fn restore(show_occasions_on_portal: bool) -> Self {
+        Self {
+            show_occasions_on_portal,
+        }
+    }
+
+    /// 企画情報の企画実施場所を参加団体に表示するかどうか
+    pub fn show_occasions_on_portal(&self) -> bool {
+        self.show_occasions_on_portal
+    }
+
+    /// 企画情報の企画実施場所を参加団体に表示するかどうかを変更
+    pub fn change_show_occasions_on_portal(&mut self, new_value: bool) {
+        self.show_occasions_on_portal = new_value;
+    }
+}
